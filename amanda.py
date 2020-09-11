e56589c40503ea178a2a8b09240f090be8733f83
amandadumit <amandadumit@gmail.com>
2020-09-11 19:04:03 +0000

    add 1 e segundo scripts em python

--- amanda.py
+++ amanda.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+import sys
 
-print ("My name: Amanda")
+name = "Amanda"
+print ("My name:", name)
+color = "red"
+print ("My favorite color:", color)
+fav_act = "reading"
+print ("My favorite activity:", fav_act)
+fav_animal = "lion"
+print ("My favorite animal:", fav_animal)
+
+print ("meu nome é", sys.argv[1], "minha cor favorita é", sys.argv[2], "minha atividade favorita é", sys.argv[3], "meu animal favorito é", sys.argv[4])
+
+print ("meu nome é" + sys.argv[1] + "minha cor favorita é" + sys.argv[2] + "minha atividade favorita é" + sys.argv[3] + "meu animal favorito é" + sys.argv[4]
+)
+
---
+++ segundo_script.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import sys
+
+print ("meu nome é:", sys.argv [1])
+
+
