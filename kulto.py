8bb6004247c83508818e4b260314dc7b84fbb954
amandadumit <amandadumit@gmail.com>
2020-09-18 00:32:02 +0000

    add scripts do joao (kulto) de if/else

---
+++ kulto.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+kulto = 'sucesso'
+
+if (kulto == 'sucesso'):
+	print ('kulto é um sucesso') 
+else:
+	print ('kulto só fala abobrinha') 
+
+
---
+++ true_false.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+valor = '0'
+
+if (valor == '0'):
+	print ('this value is TRUE')
+else:
+	print ('this value is NOT true')
+
