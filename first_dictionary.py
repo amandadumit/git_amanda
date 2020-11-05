e8c32ff2ac7f76dc4bc5294e9fedf71155156d6a
amandadumit <amandadumit@gmail.com>
2020-11-05 15:55:55 +0000

    atualizando scripts

--- first_dictionary.py
+++ first_dictionary.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python3
 
-#criando dicionário com as coisas favoritas
+#1 criando dicionário com as coisas favoritas
 
-fav_dict = { 'comida' : 'lasanha', 'banda' : 'arctic monkeys', 'flor' : 'rosa', 'organismo' : 'virus' }
+fav_dict = { 'comida' : 'lasanha', 'banda' : 'arctic monkeys', 'flor' : 'rosa', 'organismo' : 'virus', 'livro' : 'a sombra do vento', 'árvore' : 'ipê roxo' }
 
 print(fav_dict) 
 
-print(fav_dict['comida'])
+#2 imprimindo livro favorito
 
-fav_thing = 'comida'
+print(fav_dict['livro'])
+
+#3 imprimindo livro favorito usando uma variável
+
+fav_thing = 'livro'
 print(fav_dict[fav_thing])
 
-print(fav_dict['flor'])
+#4 imprimindo árvore favorita
+
+print(fav_dict['árvore'])
+
+#5 adicionando organismo favorito
 
 fav_thing = 'organismo'
 print(fav_dict[fav_thing]) 
 
-#alterando o valor de 'organismo'
+#6 favorite thing do usuario
+
+print(fav_dict.keys())
+usuario = str(input("\n write your favorite thing: "))
+print(fav_dict[usuario])
+
+#7 alterando o valor de 'organismo'
 
 fav_dict['organismo'] = 'bacteria'
 print(fav_dict) 
+
+#8 nova favorite thing do usuario 
+
+usuario = str(input("\n write your favorite thing: "))
+valor = str(input("\n write the new value for that thing: "))
+fav_dict[usuario] = valor
+print(fav_dict)
+
+#9 usando loop for para imprimir as keys e valores do dicionário
+
+for key, value in fav_dict.items() :
+    print (key," : ", value)
+
 
